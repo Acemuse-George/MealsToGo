34d1fe4c9d965f560fd271207994067c3b8de2c3
George <george@acemuse.com>
2023-10-30 23:21:22 +0800

    Fix for android spacer

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import React from "react";
+import styled, { useTheme } from "styled-components";
 
 const sizeVariant = {
   small: 1,
@@ -16,9 +17,16 @@ const positionVariant = {
 const getVariant = (position, size, theme) =>
   `${positionVariant[position]}: ${theme.space[sizeVariant[size]]};`;
 
-export const Spacer = styled.View`
-  ${({ position, size, theme }) => getVariant(position, size, theme)}
+const SpacerView = styled.View`
+  ${({ variant }) => variant}
 `;
+
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
 
 Spacer.defaultProps = {
   position: "top",
