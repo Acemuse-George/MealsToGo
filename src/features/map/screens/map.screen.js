2e934e6c8aa7998fdbd9f1c270b11e69e4156403
George <george@acemuse.com>
2023-11-15 12:48:33 +0800

    Fix warning and remove safearea

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,12 +1,16 @@
 import React from "react";
+import styled from "styled-components/native";
 import MapView from "react-native-maps";
 
-import { SafeArea } from "../../../components/utility/safe-area.component";
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
 
 export const MapScreen = () => {
   return (
-    <SafeArea>
-      <MapView style={{ height: "100%" }} />
-    </SafeArea>
+    <>
+      <Map />
+    </>
   );
 };
