7b8a8ecc031ade2fe540262cf736858e541885a7
George <george@acemuse.com>
2023-11-15 13:42:17 +0800

    Render custom map callout view

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components/native";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Callout, Marker } from "react-native-maps";
 
 import { Search } from "../components/search.component";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { MapCallout } from "../components/map-callout.component";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -46,7 +47,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <Callout>
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
           );
         })}
       </Map>
