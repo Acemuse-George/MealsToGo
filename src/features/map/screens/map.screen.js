22d38c71c972edf3631e49d17c23f4558c64c3b6
George <george@acemuse.com>
2023-11-15 12:44:19 +0800

    Add mapview

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Text } from "react-native";
+import MapView from "react-native-maps";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const MapScreen = () => {
   return (
     <SafeArea>
-      <Text>Map</Text>
+      <MapView style={{ height: "100%" }} />
     </SafeArea>
   );
 };
