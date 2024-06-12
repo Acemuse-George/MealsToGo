fd6b842b914184ab563db9c4d14d74bb4e3562a0
George <george@acemuse.com>
2024-06-12 15:21:32 +0800

    Add watermelon animation

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -4,5 +4,6 @@ import { StatusBar } from "react-native";
 //  StatusBar.currentHeight only applies to Android
 export const SafeArea = styled.SafeAreaView`
   flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px;`}
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
