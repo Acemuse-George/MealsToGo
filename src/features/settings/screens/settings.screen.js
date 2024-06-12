7253eac7b8c7ac952dbe34d64723b005178971e9
George <george@acemuse.com>
2024-06-12 12:22:53 +0800

    Add logout function in setting

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,12 +1,17 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useContext } from "react";
+import { Button, Text } from "react-native";
+
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const SettingsScreen = () => {
+  const { onLogout } = useContext(AuthenticationContext);
+
   return (
     <SafeArea>
       <Text>Settings</Text>
+      <Button title="logout" onPress={() => onLogout()} />
     </SafeArea>
   );
 };
