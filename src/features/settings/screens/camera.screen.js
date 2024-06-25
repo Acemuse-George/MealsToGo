e186cd42e1afb168c9494840a55a6b195004f3a2
George <george@acemuse.com>
2024-06-25 15:17:31 +0800

    Add take picture and open front camera

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,3 +1,47 @@
 import React from "react";
+import { Camera, CameraType } from "expo-camera";
+import styled from "styled-components/native";
+import { TouchableOpacity, View } from "react-native";
+import { Text } from "../../../components/typography/text.component";
 
-export const CameraScreen = () => null;
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = React.useState(null);
+  const cameraRef = React.useRef();
+
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+    }
+  };
+
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <TouchableOpacity onPress={snap}>
+      <ProfileCamera
+        ref={(camera) => (cameraRef.current = camera)}
+        type={CameraType.front}
+        ratio="16:9"
+      />
+    </TouchableOpacity>
+  );
+};
